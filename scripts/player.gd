8b4e9ee5ec512cdfe98c4fb6184c06a433597919
KumaStar <aryanbhatt2982@gmail.com>
2025-09-27 10:28:54 +0530

    character movements enhancements & new wall ordering

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,44 @@
 extends CharacterBody2D
 
 @export var speed: float = 250.0
+@onready var anim_sprite: AnimatedSprite2D = $body  # make sure the node path is correct
+@onready var face: AnimatedSprite2D = $face
 
 func _physics_process(delta: float) -> void:
 	var input_vector := Vector2.ZERO
 
+	# Horizontal
 	if Input.is_action_pressed("left"):
 		input_vector.x -= 1
 	elif Input.is_action_pressed("right"):
 		input_vector.x += 1
-		
-	if Input.is_action_pressed("up"):
+
+	# Vertical
+	elif Input.is_action_pressed("up"):
 		input_vector.y -= 1
 	elif Input.is_action_pressed("down"):
 		input_vector.y += 1
-		
 
+	# Move the player
 	velocity = input_vector * speed
 	move_and_slide()
 
-
-func _on_areao_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	# Handle animations
+	if input_vector.x < 0:
+		if anim_sprite.animation != "left":
+			anim_sprite.play("left")
+			face.play("left")
+			
+	elif input_vector.x > 0:
+		if anim_sprite.animation != "right":
+			anim_sprite.play("right")
+			face.play("right")
+			
+	elif input_vector.y < 0:
+		if anim_sprite.animation != "up":
+			anim_sprite.play("up")
+			face.play("up")
+	elif input_vector.y > 0:
+		if anim_sprite.animation != "down":
+			anim_sprite.play("down")
+			face.play("down")
